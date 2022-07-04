7b5c7c08ee62bf59b5b6cbb409ad08acb7c566dd
Wendel Amante <80038200+wendelamante@users.noreply.github.com>
2022-07-04 10:41:35 +0800

    Delete StreetMavens/StreetMavens directory

--- StreetMavens/StreetMavens/statics/website_info.php
+++
@@ -1,9 +0,0 @@
-<?php
-date_default_timezone_set('Asia/Manila');
-
-define('WEBSITE_TITLE', 'Street Mavens');
-define('WEBSITE_CREATOR', 'Team StreetMavs');
-define('WEBSITE_CREATOR_ALIAS', 'StreetMavs');
-
-session_start();
-?>
